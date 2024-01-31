ed961b8a26a6f6f197a681583ace6b6204d2bab3
maskelog <blitzmaske@gmail.com>
2024-01-31 23:11:27 +0900

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Layout from "./components/leyout"
+import Profile from "./routes/profile"
+import Home from "./routes/home"
+import Login from "./routes/login"
+import CreateAccount from "./routes/create-account"
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+    },
+    {
+      path: "profile",
+      element: <Profile />,
+    }
+  ],
+  },
+  {
+    path:"/login",
+    element:<Login />,
+  },
+  {
+    path:"/create-account",
+    element:<CreateAccount/>
+  },
+])
+
 function App() {
   
   return (
     <>
-      
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
 export default App
