40d216af54b990d3a7351eb9f718b19be700a6a7
maskelog <blitzmaske@gmail.com>
2024-02-02 18:31:52 +0900

    회원가입 UI 생성

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+const Form = styled.form``;
+const Input = styled.input``;
+
 export default function CreateAccount(){
-  return <h1>create account</h1>
-}
+  return (
+    <Wrapper>
+      <Form>
+        <Input name="name" placeholder="Nmae" type="text" required />
+        <Input name="email" placeholder="Email" type="email" required />
+        <Input name="password" placeholder="password" type="password" required />
+        <Input name="password" placeholder="password" type="password" required />
+        <Input type="submit" value="Create Account" />
+      </Form>
+  </Wrapper>
+);}
