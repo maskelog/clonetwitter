cbe81c34dd29dc480b986f99dafae2fc2fe86ba0
maskelog <blitzmaske@gmail.com>
2024-02-04 22:12:22 +0900

    firebase 연동 테스트

--- src/routes/create-account.tsx
+++ src/routes/create-account.tsx
@@ -1,5 +1,8 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import styled from "styled-components";
+import { auth } from "../firebase";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Wrapper = styled.div`
 height: 100%;
@@ -38,6 +41,7 @@ color: tomato;
 `;
 
 export default function CreateAccount(){
+  const navigate = useNavigate();
   const [isLoading, setLoading] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -54,12 +58,17 @@ export default function CreateAccount(){
       setPassword(value);
     }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (isLoading || name === "" || email === "" || password === "") return;
     try {
-    // create account
-    // set the name of the user.
-    // redirect to the home page
+      setLoading(true);
+      const credentials = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(credentials.user);
+      await updateProfile(credentials.user, {
+        displayName: name,
+      });
+      navigate("/");
     } catch(e) {
       // setError
     }
