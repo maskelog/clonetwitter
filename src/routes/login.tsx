4e90ec674dc874c0015373f63ff06b1aa9a870d1
maskelog <blitzmaske@gmail.com>
2024-02-06 23:39:18 +0900

    github 인증 추가

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -11,6 +11,7 @@ import {
   Title,
   Wrapper,
 } from "../components/auth-compoonents";
+import GithubButton from "../components/github-btn";
 
 export default function CreateAccount() {
   const navigate = useNavigate();
@@ -74,6 +75,7 @@ export default function CreateAccount() {
         Don't have an account?{" "}
         <Link to="/create-account">Create one &rarr;</Link>
       </Switcher>
+      <GithubButton />
     </Wrapper>
   );
 }
