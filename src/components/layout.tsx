44165e137ff14b0b7fdfe17095eb7df8b7cff100
maskelog <blitzmaske@gmail.com>
2024-03-15 13:41:20 +0900

    북마크 기능추가

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -135,6 +135,25 @@ const Layout: React.FC = () => {
             </svg>
           </MenuItem>
         </Link>
+        <Link to="/BookmarkPage">
+          <MenuItem>
+            {hasNotification && <NotificationDot />}
+            <svg
+              fill="none"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
+              />
+            </svg>
+          </MenuItem>
+        </Link>
         <MenuItem onClick={onLogOut} className="log-out">
           <svg
             fill="currentColor"
