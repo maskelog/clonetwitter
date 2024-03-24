2392f56f22773d9a963a3b62e3b885b4f97d329d
maskelog <blitzmaske@gmail.com>
2024-03-24 17:19:19 +0900

    아이콘 수정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -125,7 +125,6 @@ const Layout: React.FC = () => {
             <svg
               fill="none"
               strokeWidth={1.5}
-              stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
@@ -144,7 +143,6 @@ const Layout: React.FC = () => {
             <svg
               fill="none"
               strokeWidth={1.5}
-              stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
               aria-hidden="true"
