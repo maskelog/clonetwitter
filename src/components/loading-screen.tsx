f27377002fcdda02a6fcd09739296a256dd84323
maskelog <blitzmaske@gmail.com>
2024-04-11 15:29:19 +0900

    로딩 에니메이션 적용

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -1,16 +1,28 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export default function LoadingScreen(){
-  const Wrapper = styled.div`
-  height:100vh;
+const fadeInOut = keyframes`
+  0% { opacity: 0.1; }
+  50% { opacity: 1; }
+  100% { opacity: 0.1; }
+`;
+
+const Wrapper = styled.div`
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  `;
+`;
 
-  const Text = styled.span`
-    font-size:24px;
-  `;
+const Text = styled.span`
+  font-size: 24px;
+  color: #61dafb;
+  animation: ${fadeInOut} 2s linear infinite;
+`;
 
-  return <Wrapper><Text>Loading..</Text></Wrapper>
+export default function LoadingScreen() {
+  return (
+    <Wrapper>
+      <Text>Loading...</Text>
+    </Wrapper>
+  );
 }
