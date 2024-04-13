862a391a9836ecb125df8031d4a6501fc4547c83
maskelog <blitzmaske@gmail.com>
2024-04-13 22:03:07 +0900

    다크, 라이트 테마 대응

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -9,20 +9,20 @@ const Form = styled.form`
   flex-direction: column;
   gap: 10px;
 `;
-
 const TextArea = styled.textarea`
-  border: 2px solid white;
+  border: 2px solid ${(props) => props.theme.color};
   padding: 20px;
   border-radius: 20px;
   font-size: 16px;
-  color: white;
-  background-color: black;
+  color: ${(props) => props.theme.color};
+  background-color: ${(props) => props.theme.background};
   width: 100%;
   resize: none;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   &::placeholder {
     font-size: 16px;
+    color: ${(props) => props.theme.color};
   }
   &:focus {
     outline: none;
